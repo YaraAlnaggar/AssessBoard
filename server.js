48ee77bdb26960f38c079a6d08b1ab8ffd9a6e21
Hady Rashwan <h2rashwan@gmail.com>
2016-07-14 13:14:07 +0200

    openshift try 2

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ var contactController = require('./controllers/contact');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -86,7 +88,7 @@ if (app.get('env') === 'production') {
   });
 }
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), server_ip_address, function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
