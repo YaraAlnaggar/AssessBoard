614035a7777b978551b3408da12bba937182b5f6
Hady Rashwan <h2rashwan@gmail.com>
2016-07-14 11:57:15 +0200

    Revert "try postgrees"
    This reverts commit e7778d5ba9f60ca398280bd0c906b2a161333689.

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,11 @@ dotenv.load();
 
 
 module.exports = {
-  client: 'pg', // 'mysql' for later use
-  connection: process.env.DATABASE_URL ||  {
+  client: 'mysql',
+  connection: process.env.DATABASE_URL || {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD || "bakl", 
-    database: process.env.DB_NAME // "mega_boil"
+    database:"mega_boil" // process.env.DB_NAME
   }
 };
