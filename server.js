f621e633c2e2fb3b7c752cf700b89a78d2f413bd
Hady Rashwan <h2rashwan@gmail.com>
2016-07-14 13:29:10 +0200

    openshift another try

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ var app = express();
 
 // app.set('port', process.env.PORT || 3000);
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.use(compression());
 app.use(logger('dev'));
@@ -88,7 +89,7 @@ if (app.get('env') === 'production') {
   });
 }
 
-app.listen(app.get('port'), server_ip_address, function() {
+app.listen(server_port, server_ip_address, function() { //app.get('port')
   console.log('Express server listening on port ' + app.get('port'));
 });
 
