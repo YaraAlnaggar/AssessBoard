e429073ff5363e790262e4b94223441f5a41aa16
Hady Rashwan <h2rashwan@gmail.com>
2016-07-14 14:57:18 +0200

    try clearDB with hardcoded url

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,17 @@
 var dotenv = require('dotenv');
 
 dotenv.load();
+  	console.log(" process.env.DATABASE_URL")
 
 
 module.exports = {
   client: 'mysql',
-  connection: process.env.DATABASE_URL || {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD || "bakl", 
-    database:"mega_boil" // process.env.DB_NAME
-  }
+    connection: 'mysql://b2681ba3a288c8:759dd8c1@us-cdbr-iron-east-04.cleardb.net/heroku_2bbd03a2982c3c6?reconnect=true';
+
+  // connection: process.env.DATABASE_URL || {
+  //   host: process.env.DB_HOST,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD || "bakl", 
+  //   database: process.env.DB_NAME  //"mega_boil" // process.env.DB_NAME
+  // }
 };
