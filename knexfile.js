4ca1289e7cb5990ff3ab73725260436101a4a777
Hady Rashwan <h2rashwan@gmail.com>
2016-07-14 15:00:43 +0200

    re do

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,14 @@
 var dotenv = require('dotenv');
 
 dotenv.load();
-  	console.log(" process.env.DATABASE_URL")
 
 
 module.exports = {
   client: 'mysql',
-    connection: 'mysql://b2681ba3a288c8:759dd8c1@us-cdbr-iron-east-04.cleardb.net/heroku_2bbd03a2982c3c6?reconnect=true';
-
-  // connection: process.env.DATABASE_URL || {
-  //   host: process.env.DB_HOST,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASSWORD || "bakl", 
-  //   database: process.env.DB_NAME  //"mega_boil" // process.env.DB_NAME
-  // }
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD , 
+    database: process.env.DB_NAME  //"mega_boil" // process.env.DB_NAME
+  }
 };
