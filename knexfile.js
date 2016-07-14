33ac051f5fc7f850cacdb5760767c57c2a54cf3e
Hady Rashwan <h2rashwan@gmail.com>
2016-07-14 12:57:49 +0200

    try open shift

--- knexfile.js
+++ knexfile.js
@@ -5,10 +5,11 @@ dotenv.load();
 
 module.exports = {
   client: 'mysql',
-  connection: process.env.DATABASE_URL || {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD || "bakl", 
-    database:"mega_boil" // process.env.DB_NAME
+  connection:  "mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/" // process.env.DATABASE_URL ||
+   {
+    host: process.env.DB_HOST || "127.0.0.1" ,
+    user: process.env.DB_USER || "XWLv6dkW_feZ" ,
+    password: process.env.DB_PASSWORD ||  "admin64sJGBP" , //"bakl", 
+    database: "myapp"  // "mega_boil" // process.env.DB_NAME
   }
 };
